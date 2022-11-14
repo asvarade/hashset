e1904d4754b977c4205b441df8fd689b9e9feaa6
asvarade <abhijeetsvarade767@gmail.com>
2022-11-14 22:27:25 +0530

    changes

---
+++ Abhi.java
@@ -0,0 +1,21 @@
+package com.java.abhi;
+
+import java.util.HashSet;
+import java.util.Iterator;
+
+public class Abhi {
+	
+	public static void main(String []args) {
+		
+		HashSet hash = new HashSet();
+		
+		hash.add("glock17");
+		hash.add("ak47");
+		hash.add("12bor");
+		hash.add("45bor");
+		hash.add("410 shotgun");
+		
+		Iterator itr = hash.iterator();
+	}
+
+}
